195aae889abd58606a716a371d005143c8237404
Anton Borkov <borkov1994@gmail.com>
2021-05-05 07:58:58 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ DSN="$INPUT_REMOTE_USER@$INPUT_REMOTE_HOST"
 # Deploy.
 sh -c "rsync $SWITCHES -e '$RSH' $LOCAL_PATH $DSN:$INPUT_REMOTE_PATH"
 
-
+ssh  -t $INPUT_REMOTE_USER@$INPUT_REMOTE_HOST composer -v
 ssh  -t $INPUT_REMOTE_USER@$INPUT_REMOTE_HOST cd $INPUT_REMOTE_PATH && composer update --no-progress --no-interaction
 echo $'\n' "------ COMPOSER UPDATE ---------" $'\n'
 ssh  -t $INPUT_REMOTE_USER@$INPUT_REMOTE_HOST cd $INPUT_REMOTE_PATH && php yii migrate
